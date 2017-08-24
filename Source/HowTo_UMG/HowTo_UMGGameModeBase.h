470baf53ca59f881d0aba5faf84d6b5a07d7bb1f
mitchell.wilson <mitchell.wilson@epicgames.com>
2017-08-24 14:44:09 -0400

    Initial commit of HowTo_UMG
    Following UE4 Documentation C++ Tutorial - User Interface With UMG

---
+++ Source/HowTo_UMG/HowTo_UMGGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HowTo_UMGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOWTO_UMG_API AHowTo_UMGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
